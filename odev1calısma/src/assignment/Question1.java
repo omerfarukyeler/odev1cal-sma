cfcff4dc7d3a2c4df63d629cc6b6911b5ecbd9d3
User <User@172.17.249.242>
2022-12-08 12:23:36 +0100

    ilk commit

---
+++ odev1calısma/src/assignment/Question1.java
@@ -0,0 +1,105 @@
+package assignment;
+
+public class Question1 {
+	
+	public static void main (String[] args) {
+		
+		byte yas = 33 ;		
+		System.out.println(yas);
+		
+		byte sayı ;
+		sayı = 45;
+		System.out.println(sayı);
+		
+		byte distance = 115;
+		System.out.println(distance);
+		
+		
+		
+		short rakam = 3234;
+		System.out.println(rakam);
+		
+		short eleman_sayı =78;
+		System.out.println(eleman_sayı);
+		
+		short uc_sayısı = 32766 ;
+		System.out.println(uc_sayısı);
+		
+		
+		
+		int eleman_1 = 456;
+		System.out.println(eleman_1);
+		
+		int eleman_2 = 34;
+		System.out.println(eleman_2);
+		
+		int eleman_3 =12131231;
+		System.out.println(eleman_3);
+		
+		
+		
+		long sayı_1 =999999999;
+		System.out.println(sayı_1);
+
+		long sayı_2 =986553;
+		System.out.println(sayı_2);
+		
+		long sayı_3 =63091;
+		System.out.println(sayı_3);
+		
+		
+		
+		float rakam_88 =  1.12121212121f;
+		System.out.println(rakam_88);
+		
+		float sayı_4 = 45.12f;
+		System.out.println(sayı_4);
+		
+		float eleman_5 = 090809808098.123123123f ;
+		System.out.println(eleman_5);
+		
+		
+		
+		
+		double yas_5 = 76;
+		System.out.println(yas_5);
+		
+		double sayı_6 = 9779879.123123123;
+		System.out.println(sayı_6);
+		
+		double eleman_66 = 0.67671426;
+		System.out.println(eleman_66);
+		
+		
+		
+		char harf = 'o';
+		System.out.println(harf);
+		
+		char harf_2 ='m';
+		System.out.println(harf_2);
+		
+		char harf_3 = 'e';
+		System.out.println(harf_3);
+		
+		char harf_4='r';
+		System.out.println(harf_4);
+		
+		
+		
+		 boolean a = true;
+		 boolean b = false ;
+		 boolean c = false ;
+		 
+		System.out.println("the value of a="+a);
+		System.out.println("the value of b="+b);		          
+		System.out.println("the value of c="+c);
+		 
+				
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ odev1calısma/src/assignment/Question2.java
@@ -0,0 +1,59 @@
+package assignment;
+
+public class Question2 {
+	public static void main (String[]args) {
+		
+		
+		byte max_value = Byte.MAX_VALUE;
+		System.out.println(max_value);
+		
+		byte mın_value = Byte.MIN_VALUE ;
+		System.out.println(mın_value);
+		
+		
+		
+		short max_value_1 = Short.MAX_VALUE;
+		System.out.println(max_value_1);
+		
+		short min_value_1 = Short.MIN_VALUE;
+		System.out.println(min_value_1);
+		
+		
+		
+		int max_value_2 =Integer.MAX_VALUE ;
+		System.out.println(max_value_2);
+		
+		int mın_value_2 =Integer.MIN_VALUE;
+		System.out.println(mın_value_2);
+		
+		
+		
+		long max_value_3 = Long.MAX_VALUE;
+		System.out.println(max_value_3);
+		
+		long mın_value_3 = Long.MIN_VALUE;
+		System.out.println(mın_value_3);
+		
+		
+		
+		float max_value_4 = Float.MAX_VALUE;
+		System.out.println(max_value_4);
+		
+		float mın_value_4 =Float.MIN_VALUE;
+		System.out.println(mın_value_4);
+		
+		
+	
+		double max_value_5 =Double.MAX_VALUE;
+		System.out.println(max_value_5);
+		
+		double mın_value_5 = Double.MIN_VALUE;
+		System.out.println(mın_value_5);
+		
+		
+		
+		
+		
+	}
+
+}
